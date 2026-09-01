1a52cf4f3141cd0426745b6fe5b323b3deda324e
isa-hida <isabelhidalgoavilaa@gmail.com>
2026-09-01 10:29:05 -0400

    feat: 15 - love cuando jugador 1 anota 1 punto

--- src/tennis.js
+++ src/tennis.js
@@ -1,6 +1,16 @@
 class Tennis {
+
+    constructor() {
+        this.puntosJugador1 = 0;
+    }
     obtenerScore() {
+        if (this.puntosJugador1 === 1) {
+            return "15 - Love";
+        }
         return "Love - Love";
+    }
+    anotaPuntoJugador1() {
+        this.puntosJugador1++;
     }
 }
 export default Tennis;
--- src/tennis.spec.js
+++ src/tennis.spec.js
@@ -5,5 +5,11 @@ describe("Tennis", () => {
         let tennis = new Tennis();
         expect(tennis.obtenerScore()).toEqual("Love - Love");
     });
+
+    it("deberia mostrar 15 - love cuando jugador 1 anota 1 punto", () => {
+        let tennis = new Tennis();
+        tennis.anotaPuntoJugador1();
+        expect(tennis.obtenerScore()).toEqual("15 - Love");
+    });
 });
 
